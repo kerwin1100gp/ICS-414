0b1a74916fc3f735538919977d7476d609332352
Kevin B <beydlerk@users.noreply.github.com>
2016-11-05 01:19:09 -1000

    Update DigitalTimerObject
    Added skeleton for the object of the timer itself. It only counts incrementally. Will try to modify to descend not ascend in counting. This is a basic timer for now for demo.

--- DigitalTimerObject.java
+++ DigitalTimerObject.java
@@ -3,7 +3,7 @@
  * 
  * @author Kevin Beydler, Bryson, Kerwin 
  * @version ICS 414
- * @date 10/19/2016
+ * @date 11/2/2016
  **/
 
 //import preprocessor directives
@@ -15,18 +15,41 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class DigitalTimerObject {
+	public class DigitalTimerObject extends JLabel implements ActionListener{
+	  private SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss"); // sets the format for the Digital Timer obj
+	  private Timer timer;
+	  
+	  
+	  /*
+	   * Method: To create a basic timer JLabel digitaltimer object that counts incrementally (need to descend for project)
+	   */
+	  public DigitalTimerObject(){
+	     
+		  super();
 
-	private String hour; 						//hour data field
-	private String minute; 						//minute data field
-	private String second;						//second data field
+		  //All of the specs for the digital timer itself (i.e., its text format, the colors of the background/foreground, the speed it counts at)
+	     setText(sdf.format(new Date(System.currentTimeMillis())));
+	     setFont(new Font("Monospaced", Font.BOLD, 100));
+	     setOpaque(true);
+	     setBackground(Color.white);
+	     setForeground(Color.black);
 
-	/*
-	 * DigitalTimerObject construction method
-	 */
-	public DigitalTimerObject()
-	{
-	}
-	
-}
+	     setVisible(true);
+	  }
+	  /*
+	   * Action listener object to confirm that the action of the digital timer occurred... help was from oracle javadoc
+	   */
+	  public void actionPerformed(ActionEvent e){
+	     if(e.getSource().equals(timer))
+	        setText(sdf.format(new Date(System.currentTimeMillis())));
+	  }
+	 }
